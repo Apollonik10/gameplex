9e5aa1f11a98d2b1586bee0d72bf2f67aae335d2
Felipe C. Garcia <apollonik10@gmail.com>
2026-06-10 10:28:07 -0300

    Week 1: Install dependencies and configure PWA/Environment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+/** @type {import('next').NextPath} */
 const nextConfig = {
-  /* config options here */
+  // Configurações base do Next.js
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
